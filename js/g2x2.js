965d4948d26ac4e4a6be7f87b28dfe7bc0e64331
HARRISH <96364633+mharrish7@users.noreply.github.com>
2022-05-27 19:02:04 +0530

    Add files via upload

---
+++ js/g2x2.js
@@ -0,0 +1,127 @@
+
+
+var buttons = ['1','2','3','4'];
+var userpat = [];
+var gamepat = [];
+var level = 0;
+var levelob = document.querySelector('.level');
+var infoob = document.querySelector('.info');
+var start = false;
+
+function gamestart(){
+    gamepattern();
+    levelob.innerHTML = "Level : " + String(level);
+}
+
+document.addEventListener('keypress',function(){
+    if(start == false){
+        gamestart();
+        start = true;
+        for(var i=1;i<5;i++){
+            s = document.querySelector('.b' + String(i)).classList.remove('wrong');
+            s = document.querySelector('.b' + String(i)).classList.remove('clicked');
+        }
+        
+    }
+})
+
+
+for(var i=1;i<5;i++){
+    document.querySelector('.b'+ String(i)).addEventListener('click',function(){
+        clickanim(this.getAttribute('class'));
+        clickbut(this.innerHTML);
+    });
+}
+
+function gamepattern(){
+
+
+    if(buttons.length <=0){
+        console.log('end');
+        levelob.innerHTML = "You Won!";
+    }
+    else{
+    level++;
+    for(var i=1;i<5;i++){
+        document.querySelector('.b'+ String(i)).disabled = true;
+    }
+    infoob.innerHTML = "Wait!";
+    levelob.innerHTML = "Level : " + String(level);
+    rand = Math.floor(Math.random() * buttons.length);
+    gamepat.push(buttons[rand]);
+    buttons.splice(rand,1);
+    setTimeout(function(){
+        console.log('new');
+        for(but in gamepat){
+            animbut(but);
+        }
+    },1000);
+}
+    
+}
+
+function animbut(but){
+    setTimeout(function(){
+        console.log(gamepat[but]);
+        clickanim('b' + gamepat[but]);
+        checkbut(but);
+    },100 * (but+1));
+
+    
+}
+
+function checkbut(but){
+    if(but == gamepat.length - 1){
+        
+        setTimeout(function(){
+            for(var i=1;i<5;i++){
+                document.querySelector('.b'+ String(i)).disabled = false;
+            };
+            infoob.innerHTML = "Now Play!";
+        },1000);
+        
+        
+    }
+}
+
+function clickbut(key){
+    if(userpat.includes(key)){
+        console.log('pressed before');
+    }
+    else{
+    userpat.push(key);
+    var current = userpat.length - 1;
+    console.log(userpat);
+    console.log(gamepat);
+    if (gamepat.includes(userpat[current])){
+        if (userpat.length == gamepat.length){
+            userpat = [];
+            setTimeout(gamepattern(),1000);
+        }
+    }
+    else{
+        console.log('wrong');
+        for(var i=1;i<5;i++){
+            document.querySelector('.b'+ String(i)).disabled = true;
+        }
+        document.querySelector('.b' + key).classList.add('wrong');
+        for(but of gamepat){
+            document.querySelector('.b' + but).classList.add('clicked');
+        }
+        infoob.innerHTML = 'Wrong! press any key to restart';
+        gamepat = [];
+        userpat = [];
+        buttons = ['1','2','3','4'];
+        start = false;
+        level = 0;
+    }
+}
+}
+
+function clickanim(but){
+    var c = document.querySelector('.' + but);
+    c.classList.add('clicked');
+    setTimeout( function(){
+        c.classList.remove('clicked') 
+    },200);
+}
